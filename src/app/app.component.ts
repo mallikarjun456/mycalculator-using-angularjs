8160e5e01d906d447faad6cd039d70aab44220f9
mallikarjun456 <mallikarjuntugawe@gmail.com>
2023-12-16 02:45:19 +0530

    mycalculator-using-angularjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycalculator';
+  toshow = '0'
+  currvalue=''
+  writetoinput(value:string){
+      this.currvalue = this.currvalue + value
+      this.toshow = this.currvalue
+  }
+
+  equals(){
+    this.toshow = eval(this.currvalue)
+    this.currvalue = this.toshow
+  }
+
+  clear(){
+    this.currvalue='';
+    this.toshow='0';
+  }
+
+  back(){
+    this.currvalue = this.currvalue.slice(0,-1)
+    this.toshow = this.currvalue
+    if (this.toshow=='') {this.toshow='0'}
+
+  }
+
+  calcvalue(solve:any){
+    this.toshow = eval(solve)
+  }
 }
